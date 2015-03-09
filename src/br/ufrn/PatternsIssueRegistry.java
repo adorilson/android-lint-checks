97ac29bff5eb60b65baaf75a349692d0034355e2
Adorilson Bezerra <adorilson@ppgsc.ufrn.br>
2015-03-09 20:22:16 -0300

    Adding the issue registry

---
+++ src/br/ufrn/PatternsIssueRegistry.java
@@ -0,0 +1,18 @@
+package br.ufrn;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.Issue;
+
+public class PatternsIssueRegistry extends IssueRegistry {
+
+	@Override
+	public List<Issue> getIssues() {
+		return Arrays.asList(
+	            PatternsDetector.CHECKFRAGMENTACTIVITY
+	        );
+	}
+
+}
