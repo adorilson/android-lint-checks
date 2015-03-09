6126bee65e03f7333464fb5c693307b5633ce69f
Adorilson Bezerra <adorilson@ppgsc.ufrn.br>
2015-03-09 19:59:56 -0300

    Creating the AST visitor that check if the main activity extends from FragmentActivity class

--- src/br/ufrn/PatternsDetector.java
+++ src/br/ufrn/PatternsDetector.java
@@ -8,6 +8,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumSet;
 
+import lombok.ast.AstVisitor;
+import lombok.ast.ClassDeclaration;
+import lombok.ast.ForwardingAstVisitor;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -16,6 +20,8 @@ import com.android.tools.lint.detector.api.Detector.JavaScanner;
 import com.android.tools.lint.detector.api.Category;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.Location;
 import com.android.tools.lint.detector.api.ResourceXmlDetector;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
@@ -54,4 +60,35 @@ public class PatternsDetector extends ResourceXmlDetector implements JavaScanner
     	int lastDot = mainActivity.getNodeValue().lastIndexOf('.');
     	this.mainActivity = mainActivity.getNodeValue().substring(lastDot+1);
     }
+
+	@Override
+	public AstVisitor createJavaVisitor(JavaContext context) {
+		return new PerformanceVisitor(context, this.mainActivity);
+	}
+
+	private static class PerformanceVisitor extends ForwardingAstVisitor {
+		private final JavaContext mContext;
+		private String ACTIVITY;
+
+		public PerformanceVisitor(JavaContext context, String activity) {
+			mContext = context;
+			this.ACTIVITY = activity;
+		}
+
+		@Override
+		public boolean visitClassDeclaration(ClassDeclaration node) {
+			if (node.astName().toString().equals(ACTIVITY)){
+				if (!node.astExtending().toString().equals(FRAGMENT)){
+					report(node);
+				}
+			}
+			return super.visitClassDeclaration(node);
+		}
+
+		private void report(ClassDeclaration node) {
+			String message = ACTIVITY + " class should extends " + FRAGMENT;
+			Location location = mContext.getLocation(node.astName());
+			mContext.report(CHECKFRAGMENTACTIVITY, node, location, message);
+		}
+	}
 }
